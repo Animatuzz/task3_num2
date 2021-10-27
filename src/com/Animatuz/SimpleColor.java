002db9a46f997cb0ae553549160c28af5870a094
Animatuzz <suvorik118@gmail.com>
2021-10-27 13:45:22 +0300

    v3

---
+++ src/com/Animatuz/SimpleColor.java
@@ -0,0 +1,14 @@
+package com.Animatuz;
+
+public enum SimpleColor {
+
+    BLACK,
+    WHITE,
+    GRAY,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE;
+
+}
---
+++ src/com/Animatuz/VerticalLine.java
@@ -0,0 +1,17 @@
+package com.Animatuz;
+
+public class VerticalLine {
+
+    public double x0;
+
+
+    public VerticalLine(double x0) {
+
+        this.x0 = x0;
+
+    }
+
+    public boolean isPointRight(double x) {
+        return x > x0;
+    }
+}
